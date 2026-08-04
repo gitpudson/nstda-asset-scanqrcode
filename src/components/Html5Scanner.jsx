3035f40b8ac372a35deaecc209de391152b8e96c
@pudson <pichet.pudson@nectec.or.th>
2026-08-04 09:07:15 +0700

    Update

--- src/components/Html5Scanner.jsx
+++ src/components/Html5Scanner.jsx
@@ -4,6 +4,7 @@ import {
   Html5QrcodeSupportedFormats,
 } from "html5-qrcode";
 
+/*
 export default function Html5Scanner({ onScanSuccess }) {
   useEffect(() => {
     let scanner = null;
@@ -49,6 +50,85 @@ export default function Html5Scanner({ onScanSuccess }) {
 
           (decodedText) => {
             onScanSuccess(decodedText);
+          },
+
+          () => {}
+        );
+
+        started = true;
+      } catch (error) {
+        console.error("Scanner Error:", error);
+      }
+    };
+
+    startScanner();
+
+    return () => {
+      if (scanner && started) {
+        scanner
+          .stop()
+          .then(() => scanner.clear())
+          .catch(() => {});
+      }
+    };
+  }, [onScanSuccess]);
+
+  return <div id="reader"></div>;
+}
+*/
+
+import { useEffect } from "react";
+import {
+  Html5Qrcode,
+  Html5QrcodeSupportedFormats,
+} from "html5-qrcode";
+
+export default function Html5Scanner({ onScanSuccess }) {
+  useEffect(() => {
+    let scanner = null;
+    let started = false;
+
+    const startScanner = async () => {
+      try {
+        scanner = new Html5Qrcode("reader");
+
+        const isMobile =
+          /Android|iPhone|iPad|iPod/i.test(
+            navigator.userAgent
+          );
+
+        const cameraConfig = isMobile
+          ? {
+              facingMode: "environment",
+            }
+          : {
+              facingMode: "user",
+            };
+
+        await scanner.start(
+          cameraConfig,
+          {
+            fps: 5,
+
+            qrbox: {
+              width: 280,
+              height: 280,
+            },
+
+            aspectRatio: 1,
+
+            formatsToSupport: [
+              Html5QrcodeSupportedFormats.QR_CODE,
+            ],
+          },
+
+          (decodedText) => {
+            onScanSuccess(decodedText);
+
+            scanner
+              .stop()
+              .then(() => scanner.clear())
+              .catch(() => {});
           },
 
           () => {}
