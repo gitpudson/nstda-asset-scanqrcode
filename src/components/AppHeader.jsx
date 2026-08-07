fa2a65427bbfadb4d5aa10c08fe5c05aadf65a35
@pudson <pichet.pudson@nectec.or.th>
2026-08-07 08:19:34 +0700

    Update

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,9 +4,12 @@ import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
 export default function AppHeader() {
   return (
     <div className="header">
-      <h2>ระบบตรวจสอบครุภัณฑ์</h2>
-
+      
       <div className="budget-chip">
+        <h2>ระบบตรวจสอบครุภัณฑ์</h2>
+      </div>
+
+      <div className="budget-chip1">
         <CalendarMonthIcon fontSize="small" />
         <span>{`สวทช. ปีงบประมาณ ${new Date().getFullYear() + 543}`}</span>
       </div>
